a914de8d4ac183aeea38edbcd51a445a34b51e50
Michele De Sena <mic.desena@studenti.unina.it>
2019-03-29 15:31:52 +0100

    Initial Commit

---
+++ Venice/Venice.h
@@ -0,0 +1,19 @@
+//
+//  Venice.h
+//  Venice
+//
+//  Created by Michele De Sena on 29/03/2019.
+//  Copyright © 2019 Michele De Sena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Venice.
+FOUNDATION_EXPORT double VeniceVersionNumber;
+
+//! Project version string for Venice.
+FOUNDATION_EXPORT const unsigned char VeniceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Venice/PublicHeader.h>
+
+
